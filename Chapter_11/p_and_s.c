1316a1a9a95e8b713510b306d63cc2a364073d23
Ashesreborn <github.madm@gmail.com>
2018-04-10 21:19:55 +0800

    Chapter_11 part one

---
+++ Chapter_11/p_and_s.c
@@ -0,0 +1,14 @@
+/* p_and_s.c -- Ö¸ÕëºÍ×Ö·û´® */
+#include <stdio.h>
+int main(Void)
+{
+	const char * mesg = "Don't be a fool!";
+	const char * copy;
+	
+	copy = mesg;
+	printf("%s\n", copy);
+	printf("mesg = %s; &mesg = %p; value = %p\n", mesg, &mesg, mesg);
+	printf("copy = %s; &copy = %p; value = %p\n", copy, &copy, copy);
+	
+	return 0;
+}
